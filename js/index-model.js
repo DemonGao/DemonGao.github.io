f9bd4ec30d2750d095b11a8547d5e05db41c1930
DemonGao <a750229099@qq.com>
2016-07-27 17:25:56 +0800

    添加日志-家的气息

--- js/index-model.js
+++ js/index-model.js
@@ -39,29 +39,31 @@ surfaced.init();
  */
 var articlescrapData = [
 	{
-		src: 'img/articlescrap/61baby.jpg',
-		title: '六·一快乐',
-		text: ' 谁说我们不能过6.1儿童节,我们萌着呢!',
-		type_href:'www.个人博客.com',
-		type_href_title:'个人博客',
-		time:'2016-06-01',
-	}, 
+		src: 'img/articlescrap/xingqin.jpg',
+		title: '家的气息',
+		text: '第一次在外感受到家的气息!<br><span class="space2"></span>很高兴在一个陌生的城市<br/><span class="space4"></span>能够认识你们...	',
+		type_href:'#',
+		type_href_title:'心情故事',
+		time:'2016-07-21',
+	},
 	{
-		src: 'img/articlescrap/IMG_6066.JPG',
+		src: 'img/articlescrap/me.JPG',
 		title: 'About Me',
-		text: '高世超,男,一个90后程序员,从小学的时候,我就对计算机比较感兴趣,当时参加了学校组织的网页制作校本课程,那时候用的是dreamweaver,都是些托拉拽的操作,那时候还不知道js,只会从网站上赋值粘贴以下代码,然后鼠标后面有了小尾巴,页面上下起了雪花,各种特效...页面布局的时候,还是用table来规划布局,根本就不知道用div+css布局,直到2014年接触了前端,我才终于知道那是html css javascript!2014年接触前端,对前端也有了进一步的了解,学校里开了Web程序设计,老师讲了一些基础知识,如:html,css,javascript,也将我带入了前端世界。2015年,开始了我自学前端的路程...自学了HTML5 CSS3 javaScript设计模式,用bootstrap+jsp+...',
-		type_href:'www.个人博客.com',
+		text: '高世超,男,一个90后程序员,从小学的时候,我就对计算机比较感兴趣,当时参加了学校组织的网页制作校本课程,那时候用的是dreamweaver,都是些托拉拽的操作,那时候还不知道js,只会从网站上赋值粘贴以下代码,然后鼠标后面有了小尾巴,页面上下起了雪花,各种特效...页面布局的时候,还是用table来规划布局,根本就不知道用div+css布局,直到2014年接触了前端...',
+		type_href:'#',
 		type_href_title:'个人博客',
 		time:'2016-05-29',
 	}, 
-	{
-		src: 'img/articlescrap/01.jpg',
-		title: '住在手机里的朋友',
-		text: '通信时代，无论是初次相见还是老友重逢，交换联系方式，常常是彼此交换名片，然后郑重或是出于礼貌用手机记下对方的电话号码。在快节奏的生活里，我们不知不觉中就成为住在别人手机里的朋友。又因某些意外，变成了别人手机里匆忙的过客，这种快餐式的友谊 ...',
-		type_href:'www.个人博客.com',
-		type_href_title:'个人博客',
-		time:'2016-05-28',
-	}
+//	{
+//		src: 'img/articlescrap/01.jpg',
+//		title: '住在手机里的朋友',
+//		text: '通信时代，无论是初次相见还是老友重逢，交换联系方式，常常是彼此交换名片，然后郑重或是出于礼貌用手机记下对方的电话号码。在快节奏的生活里，我们不知不觉中就成为住在别人手机里的朋友。又因某些意外，变成了别人手机里匆忙的过客，这种快餐式的友谊 ...',
+//		type_href:'#',
+//		type_href_title:'个人博客',
+//		time:'2016-05-28',
+//	}
+	
+	
 ];
 ViewCommand({
 	//参数说明 方法display
